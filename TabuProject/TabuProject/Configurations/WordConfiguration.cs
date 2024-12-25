7e3085e2464582ac1c79cb2c03160203bff1a713
xaqan05 <xaqan2005@gmail.com>
2024-12-25 18:50:31 +0400

    Add Congurations with different classes and import them in Data Access Layer

---
+++ TabuProject/TabuProject/Configurations/WordConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TabuProject.Entities;
+
+namespace TabuProject.Configurations
+{
+    public class WordConfiguration : IEntityTypeConfiguration<Word>
+    {
+        public void Configure(EntityTypeBuilder<Word> builder)
+        {
+            builder.Property(x => x.Text)
+                .IsRequired()
+                .HasMaxLength(32);
+            builder.HasOne(x => x.Language)
+                .WithMany(x => x.Words)
+                .HasForeignKey(x => x.LangCode);
+            builder.HasMany(x => x.BannedWords)
+                .WithOne(x => x.Word)
+                .HasForeignKey(x => x.WordId);
+        }
+    }
+}
--- TabuProject/TabuProject/DTOs/BannedWord/BannedWordCreateDto.cs
+++ TabuProject/TabuProject/DTOs/BannedWord/BannedWordCreateDto.cs
@@ -4,7 +4,7 @@ namespace TabuProject.DTOs.BannedWord
 	public class BannedWordCreateDto
 	{
         public int Id { get; set; }
-        public string Text { get; set; }
+        public string Text { get; set; } 
         public int WordId { get; set; }
     }
 }
